29d75ef2fc4aabae086e7a960748db542c81e3d8
Luis López <lucho2143103@gmail.com>
2026-02-02 10:35:23 -0500

    fix: Se empiezan a recibir los campos previamente añadidos

--- modulo_formulario/src/interface/endpoint.py
+++ modulo_formulario/src/interface/endpoint.py
@@ -8,16 +8,89 @@ from rest_framework import status
 
 from rest_framework.request import Request
 
+from ..domain.dtos import InformacionPersonaDTO
+
+import uuid
 
 class UploadFormRecordView(APIView):
 
     def post(self, request:Request):
 
-        num_documento_value = request.POST.get("num_documento")
-        foto_frontal_value = request.FILES.get("foto_frontal")
-        foto_reverso_value = request.FILES.get("foto_reverso")
-    
-     
+        id_persona_value = str(uuid.uuid4())
+        
+        # Identificación personal
+        primer_nombre_value = request.POST.get("primer_nombre")
+        segundo_nombre_value = request.POST.get("segundo_nombre")
+        primer_apellido_value = request.POST.get("primer_apellido")
+        segundo_apellido_value = request.POST.get("segundo_apellido")
+        numero_documento_value = request.POST.get("numero_documento")
+        tipo_documento_value = request.POST.get("tipo_documento")
+
+        # Información Sociodemográfica
+        estado_civil_value = request.POST.get("estado_civil")
+        direccion_residencia_value = request.POST.get("direccion_residencia")
+        zona_value = request.POST.get("zona")
+        barrio_value = request.POST.get("barrio")
+        localidad_value = request.POST.get("localidad")
+        departamento_residencia_value = request.POST.get("departamento_residencia")
+        municipio_residencia_value = request.POST.get("municipio_residencia")
+        estrato_value = request.POST.get("estrato")
+
+        # Información de Contacto
+        correo_electronico_value = request.POST.get("correo_electronico")
+        numero_celular_value = request.POST.get("numero_celular")
+        telefono_fijo_value = request.POST.get("telefono_fijo")
+
+        # Información Laboral
+        departamento_laboral_value = request.POST.get("departamento_laboral")
+        municipio_laboral_value = request.POST.get("municipio_laboral")
+        secretaria_value = request.POST.get("secretaria")
+        institucion_educativa_value = request.POST.get("institucion_educativa")
+
+        # Información de Caracterización
+        discapacidad_value = request.POST.get("discapacidad")
+        tipo_discapacidad_value = request.POST.get("tipo_discapacidad")
+        grupo_etnico_value = request.POST.get("grupo_etnico")
+        poblacion_lgbtiq_value = request.POST.get("poblacion_lgbtiq")
+
+        informacion_cotizante_dto = InformacionPersonaDTO(
+            id_persona=id_persona_value,
+
+            # Identificación personal
+            primer_nombre=primer_nombre_value,
+            segundo_nombre=segundo_nombre_value,
+            primer_apellido=primer_apellido_value,
+            segundo_apellido=segundo_apellido_value,
+            numero_documento=numero_documento_value,
+            tipo_documento=tipo_documento_value,
+
+            # Información Sociodemográfica
+            estado_civil=estado_civil_value,
+            direccion_residencia=direccion_residencia_value,
+            zona=zona_value,
+            barrio=barrio_value,
+            localidad=localidad_value,
+            departamento_residencia=departamento_residencia_value,
+            municipio_residencia=municipio_residencia_value,
+            estrato=estrato_value,
+
+            # Información de Contacto
+            correo_electronico=correo_electronico_value,
+            numero_celular=numero_celular_value,
+            telefono_fijo=telefono_fijo_value,
+
+            # Información Laboral
+            departamento_laboral=departamento_laboral_value,
+            municipio_laboral=municipio_laboral_value,
+            secretaria=secretaria_value,
+            institucion_educativa=institucion_educativa_value,
+
+            # Información de Caracterización
+            discapacidad=discapacidad_value,
+            tipo_discapacidad=tipo_discapacidad_value,
+            grupo_etnico=grupo_etnico_value,
+            poblacion_lgbtiq=poblacion_lgbtiq_value
+        )
 
         return Response(
             {
@@ -27,11 +100,4 @@ class UploadFormRecordView(APIView):
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-
-
-    
-    
-
-
-
 
