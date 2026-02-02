63bffcbc9707031f31f1da67683ac35be9c8d4aa
Luis López <lucho2143103@gmail.com>
2026-02-02 10:07:29 -0500

    fix: Se eliminan archivos no relevantes para el modulo de base de datos

--- modulo_database/src/interface/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from modulo_validacion.src.interface.endpoint import UploadFileView
-urlpatterns = [
-    path('verify-id/', UploadFileView.as_view()),
-]
